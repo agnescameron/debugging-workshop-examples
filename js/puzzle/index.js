9acb887aa93818eb5042fbe48b7e30d8cef3f748
Agnes Cameron <agnesfcameron@gmail.com>
2024-04-17 12:02:42 +0100

    added code to fix

---
+++ js/puzzle/index.js
@@ -0,0 +1,11 @@
+function toggleShaba() {
+	const shabaImg = document.getElementByID("shaba");
+
+	if(rabbitDiv.style.display === "none"){
+		rabbitDiv.style.display = "none";
+	}
+	else (rabbitDiv.style.display = "block") {
+		rabbitDiv.style.display = "block";
+	}
+
+}
